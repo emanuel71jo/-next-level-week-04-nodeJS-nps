c4e4c224d570dbd9e87b4ed83832e44a4ae59b44
emanuel71jo <emanuel71jo@gmail.com>
2021-02-25 10:44:24 -0300

    Terceiro dia do nlw 4, trilha nodeJS

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from "express";
-import { getRepository } from "typeorm";
-import { User } from "../models/User";
+import { getCustomRepository } from "typeorm";
+import { UserRepository } from "../repositories/UserRepository";
 
 class UserController {
   async create(request: Request, response: Response) {
     const { name, email } = request.body;
 
-    const userRepository = getRepository(User);
+    const userRepository = getCustomRepository(UserRepository);
 
     const userAlreadyExists = await userRepository.findOne({ email });
 
@@ -20,7 +20,7 @@ class UserController {
 
     await userRepository.save(user);
 
-    return response.send();
+    return response.status(201).json(user);
   }
 }
 
